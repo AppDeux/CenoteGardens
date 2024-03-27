7db65441e95f634da042a2fa8f592ec737d15c78
Jonathan Alexis Bello Lopez <jonathan.bellofg@gmail.com>
2024-03-27 16:00:59 -0600

    cambio de archivo de eslint creado

---
+++ .eslintrc.cjs
@@ -0,0 +1,40 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:astro/recommended",
+    "plugin:vue/recommended",
+    "plugin:prettier/recommended",
+  ],
+  plugins: ["react", "vue", "astro", "prettier"],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: [".eslintrc.{js,cjs}"],
+      parserOptions: {
+        sourceType: "script",
+      },
+    },
+    {
+      files: ["*.astro"],
+      parser: "astro-eslint-parser",
+      parserOptions: {
+        parser: "@typescript-eslint/parser",
+        extraFileExtensions: [".astro"],
+      },
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  rules: {
+    semi: ["error", "always"],
+  },
+};
---
+++ src/pages/bob.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const bob = () => {
+  const bob = {
+    b1: "2",
+    b2: "2",
+    b3: "2",
+  };
+
+  return <div>bob</div>;
+};
+
+export default bob;
