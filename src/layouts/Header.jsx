6a56015def32acfac4d3468fc0094a8e4a789459
Jonathan Alexis Bello Lopez <jonathan.bellofg@gmail.com>
2024-04-02 15:32:34 -0600

    style: add style for header

--- src/layouts/Header.jsx
+++ src/layouts/Header.jsx
@@ -22,18 +22,18 @@ const Header = () => {
   return (
     <header
       className={`Header
-      ${showMobileMenu || scrollNumber > 10 ? "bg-accent-700" : "bg-transparent"}
+      ${showMobileMenu || scrollNumber > 20 ? "bg-accent-700" : "bg-transparent"}
       fixed top-0 z-20 w-full
       transition-colors delay-300 ease-linear`}
     >
       <div
         className="Header__content
         container m-auto
-        py-3 px-2 md:pr-0
-        grid grid-cols-6 gap-2
+        py-3 px-2 lg:pr-0
+        grid grid-cols-6 lg:grid-cols-12 gap-2 lg:gap-0
         items-center justify-items-center"
       >
-        <div className="flex order-1">
+        <div className="flex order-1 lg:hidden">
           <Hamburger
             size={28}
             color="#fff"
@@ -47,10 +47,10 @@ const Header = () => {
           />
         </div>
 
-        <div className="order-2 col-span-3">
+        <div className="order-2 lg:order-1 col-span-3 lg:col-span-3">
           <img
-            className={`max-w-48 w-full
-            ${showMobileMenu || scrollNumber > 10 ? "opacity-100" : "opacity-0"}
+            className={`max-w-48 w-full lg:max-w-52
+            ${showMobileMenu || scrollNumber > 20 ? "opacity-100" : "opacity-0"}
             transition-opacity delay-300 ease-linear`}
             src={logoHeader.src}
             alt="logo cenotes"
@@ -58,9 +58,11 @@ const Header = () => {
         </div>
 
         <div
-          className={`Header__content__links
+          className={`Header__content__links font-subtitle text-grey-50
             ${showMobileMenu ? "Header__content__links--active" : "hidden"}
-            transition-all delay-300 ease-in`}
+            transition-all delay-300 ease-in
+            lg:order-2 lg:col-span-7 w-full
+            lg:flex lg:items-center lg:justify-evenly lg:gap-8`}
         >
           <a href="#">Home</a>
           <a href="#location">Location</a>
@@ -71,9 +73,9 @@ const Header = () => {
         </div>
 
         <div
-          className="flex gap-4 order-3 col-span-2
+          className="flex gap-4 order-3 col-span-2 lg:col-span-2
             justify-self-end
-            font-semibold text-xl text-grey-50"
+            font-semibold text-xl lg:text-lg text-grey-50"
         >
           <a>EN</a> | <a>ES</a>
         </div>
