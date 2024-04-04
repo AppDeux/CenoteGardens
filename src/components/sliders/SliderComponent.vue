6f4b1b59b22767b5a69bdbd9faa4491f3e69a9be
Angel Gasca <angel.cesar@onexo.mx>
2024-04-04 12:33:34 -0600

    changes: slider styles

--- src/components/sliders/SliderComponent.vue
+++ src/components/sliders/SliderComponent.vue
@@ -1,32 +1,59 @@
 <template>
-  <swiper :navigation="true" :modules="modules" class="mySwiper">
-    <swiper-slide>Slide 1</swiper-slide>
-    <swiper-slide>Slide 2</swiper-slide><swiper-slide>Slide 3</swiper-slide>
-    <swiper-slide>Slide 4</swiper-slide><swiper-slide>Slide 5</swiper-slide>
-    <swiper-slide>Slide 6</swiper-slide><swiper-slide>Slide 7</swiper-slide>
-    <swiper-slide>Slide 8</swiper-slide><swiper-slide>Slide 9</swiper-slide>
+  <swiper :navigation="true" :modules="modules" class="mySwiper my-5">
+    <swiper-slide v-for="(item, index) in items" :key="index">
+      <div class="slider-content">
+        <img :src="item.pathImg.src" alt="" />
+        <p>{{ item.pathImg.src }}</p>
+      </div>
+    </swiper-slide>
   </swiper>
 </template>
 <script>
 // Import Swiper Vue.js components
 import { Swiper, SwiperSlide } from "swiper/vue";
-
 // Import Swiper styles
 import "swiper/css";
 import "swiper/css/navigation";
-import "./style.css";
-
 // import required modules
 import { Navigation } from "swiper/modules";
+// import images for slider
+import img1 from "@/assets/imgs/Foto_1.png";
+import img2 from "@/assets/imgs/Foto_2.png";
+import img3 from "@/assets/imgs/Foto_3.png";
+import img4 from "@/assets/imgs/Foto_4.png";
 
 export default {
   components: {
     Swiper,
     SwiperSlide,
   },
+  props: {
+    arrayItems: {
+      type: Array,
+      value: [],
+    },
+  },
   setup() {
     return {
       modules: [Navigation],
+    };
+  },
+  data() {
+    return {
+      items: [
+        {
+          pathImg: img1,
+        },
+        {
+          pathImg: img2,
+        },
+        {
+          pathImg: img3,
+        },
+        {
+          pathImg: img4,
+        },
+      ],
     };
   },
 };
