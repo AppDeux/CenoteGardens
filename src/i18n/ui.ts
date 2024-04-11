d7f299a754f2d790eb7781db5b18ed16aa4d378c
Jonathan Alexis Bello Lopez <jonathan.bellofg@gmail.com>
2024-04-11 12:21:12 -0600

    add: translate for description section

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -14,8 +14,26 @@ export const ui = {
     "nav.invest": "Why invest in tulum?",
     "nav.blog": "Blog",
     "nav.contact": "Contact",
-    "descriptionSection.about": `Escape to a world where nature embraces contemporary comfort—nestled in
-    the vibrant jungles of Tulum.`,
+    "descriptionSection.title": "Residential development in Tulum",
+    "descriptionSection.about": `Escape to a world where nature embraces contemporary comfort—nestled in the vibrant jungles of Tulum.`,
+    "descriptionSection.about2": `Picture waking up to the beautiful songs of tropical birds, surrounded by the lush jungle in vibrant colors. Every day is like a new adventure waiting to happen.`,
+    "descriptionSection.aboutSecond":
+      "Cenote Gardens offers <b>56 residential lots</b> that bring a unique opportunity to be part of an extraordinary living experience.",
+    "descriptionSection.aboutSecond2":
+      "It is the perfect place for those looking for <b>a lifestyle respectful of nature</b> and the biodiversity of the Mayan jungle.",
+    "descriptionSection.aboutSecond3":
+      "Ideal to find <b>privacy</b> and coexistence with other people seeking wellness, spirituality, and expansion of consciousness.",
+    "sustainability.title": "Sustainability is our core vision",
+    "sustainability.card1":
+      "Low construction density (+60% of untouched green areas).",
+    "sustainability.card2":
+      "Private eco-friendly sewage system that completes the water cycle (water reuse).",
+    "sustainability.card3":
+      "Renewable energy for the lighting of driveways and power of amenities.",
+    "sustainability.card4":
+      "Amenities and driveways designed with eco-friendly materials.",
+    "sustainability.card5": "Solar panel buyer rebate program.",
+    "sustainability.card6": "Eco-construction guidelines.",
   },
   es: {
     "nav.home": "Inicio",
@@ -23,6 +41,26 @@ export const ui = {
     "nav.availability": "Disponibilidad",
     "nav.invest": "¿Por quÉ invertir en Tulum?",
     "nav.contact": "Contacto",
+    "descriptionSection.title": "Desarrollo residencial en Tulum",
     "descriptionSection.about": `Escápate a un mundo en donde la naturaleza de la selva maya abraza el confort contemporáneo.`,
+    "descriptionSection.about2": `Imagínate despertar y escuchar los cantos de las aves y rodearte de colores vibrantes. Cada día es una nueva aventura esperando ser vivida.`,
+    "descriptionSection.aboutSecond":
+      "Cenote Gardens ofrece <b>56 lotes residenciales</b> que te permiten tener una experiencia de vida extraordinaria.",
+    "descriptionSection.aboutSecond2":
+      "Es el lugar perfecto para aquellos que buscan <b>un estilo de vida respetuoso con la naturaleza</b> y la biodiversidad de la selva maya.",
+    "descriptionSection.aboutSecond3":
+      "Ideal para encontrar <b>privacidad</b> y convivencia con otras personas que buscan bienestar, espiritualidad y expansión de la conciencia.",
+    "sustainability.title": "La sostenibilidad es nuestra visión central",
+    "sustainability.card1":
+      "Baja densidad de construcción (+60% de áreas verdes vírgenes).",
+    "sustainability.card2":
+      "Sistema de alcantarillado ecológico privado que completa el ciclo del agua (reutilización del agua).",
+    "sustainability.card3":
+      "Energías renovables para el alumbrado de calzadas y la energía de los servicios.",
+    "sustainability.card4":
+      "Instalaciones y accesos diseñados con materiales ecológicos.",
+    "sustainability.card5":
+      "Programa de descuento comprador de paneles solares.",
+    "sustainability.card6": "Lineamientos de construcción ecológica.",
   },
 } as const;
