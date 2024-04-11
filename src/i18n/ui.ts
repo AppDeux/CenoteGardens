693f0b1d2c5bb1f09875ff18ce371b49d39be55c
Jonathan Alexis Bello Lopez <jonathan.bellofg@gmail.com>
2024-04-10 20:26:33 -0600

    add: files for i18n

---
+++ src/i18n/ui.ts
@@ -0,0 +1,19 @@
+export const languages = {
+  en: "English",
+  es: "Español",
+};
+
+export const defaultLang = "es";
+
+export const ui = {
+  en: {
+    "nav.home": "Home",
+    "nav.about": "About",
+    "descriptionSection.about": `Escape to a world where nature embraces contemporary comfort—nestled in
+    the vibrant jungles of Tulum.`,
+  },
+  es: {
+    "nav.home": "Accueil",
+    "descriptionSection.about": `Escápate a un mundo en donde la naturaleza de la selva maya abraza el confort contemporáneo.`,
+  },
+} as const;
