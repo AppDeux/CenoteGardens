e6ee1f64916902256c64c68971a4ad58cb726015
Jonathan Alexis Bello Lopez <jonathan.bellofg@gmail.com>
2024-04-23 23:17:01 -0600

    add: change to hybrid

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import netlify from "@astrojs/netlify";
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), vue(), tailwind()],
-  output: "server",
+  output: "hybrid",
   adapter: netlify({
     cacheOnDemandPages: true,
   }),
