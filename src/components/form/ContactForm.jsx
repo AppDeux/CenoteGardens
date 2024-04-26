40a128be46df60d0112f89713ea7df2b09ef1c8e
Jonathan Alexis Bello Lopez <jonathan.bellofg@gmail.com>
2024-04-25 23:16:59 -0600

    add new form for contact[

--- src/components/form/ContactForm.jsx
+++ src/components/form/ContactForm.jsx
@@ -1,44 +1,27 @@
-import { useEffect } from "react";
+const FormEmbed = ({ lang }) => {
+  const formID =
+    lang === "en"
+      ? "20ea62af-851f-404d-85f2-3c54d4e8dcb0"
+      : "97b84d5a-6b1c-430d-a867-7d85e0205f93";
 
-const HubSpotForm = ({ lang }) => {
-  useEffect(() => {
-    const loadHubSpotForm = () => {
-      const script = document.createElement("script");
-      script.src = `//js.hsforms.net/forms/embed/v2.js`;
-      script.async = true;
-      document.getElementById("hubspot-form-container").appendChild(script);
-
-      script.onload = () => {
-        hbspt.forms.create({
-          region: "na1",
-          portalId: "44724690",
-          formId:
-            lang === "en"
-              ? "20ea62af-851f-404d-85f2-3c54d4e8dcb0"
-              : "97b84d5a-6b1c-430d-a867-7d85e0205f93",
-        });
-      };
-    };
-
-    const observer = new IntersectionObserver((entries) => {
-      entries.forEach((entry) => {
-        if (entry.isIntersecting) {
-          loadHubSpotForm();
-          observer.disconnect(); // Desconecta el observador una vez que el formulario se ha cargado
-        }
-      });
-    });
-
-    const target = document.getElementById("hubspot-form-container");
-    if (target) {
-      observer.observe(target);
-    }
-
-    // Limpia el observador al desmontar el componente
-    return () => observer.disconnect();
-  }, [lang]);
-
-  return <div id="hubspot-form-container"></div>;
+  return (
+    <div
+      className="flex justify-center"
+      dangerouslySetInnerHTML={{
+        __html: `<script charset="utf-8" type="text/javascript" src="//js.hsforms.net/forms/embed/v2.js"></script>
+        <script>
+          hbspt.forms.create({
+            region: "na1",
+            portalId: "44724690",
+            formId: "${formID}"
+          });
+        </script>
+        `,
+      }}
+    >
+      {/* Aquí puedes colocar contenido adicional si es necesario */}
+    </div>
+  );
 };
 
-export default HubSpotForm;
+export default FormEmbed;
