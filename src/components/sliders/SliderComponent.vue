27f993f93e54f044c0f6d2251fe40ebcdf8f2ac5
Jonathan Alexis Bello Lopez <jonathan.bellofg@gmail.com>
2024-04-26 23:20:59 -0600

    see what images variants nedd for carousel

--- src/components/sliders/SliderComponent.vue
+++ src/components/sliders/SliderComponent.vue
@@ -9,8 +9,16 @@
     <swiper-slide v-for="(item, index) in items" :key="index">
       <div class="slider-content">
         <img
-          :src="item.pathImgMobile.src"
-          :srcset="`${item.pathImgMobile.src} 400w, ${item.pathImg.src} 960w`"
+          :src="item.pathImg960.src"
+          :srcset="`${item.pathImg256.src} 256w,
+           ${item.pathImg460.src} 460w,
+           ${item.pathImg960.src} 960w,
+           ${item.pathImg1472.src} 1472w,
+           ${item.pathImg1710.src} 1710w,
+           ${item.pathImg1920.src} 1920w,
+           ${item.pathImg2048.src} 2048w,
+           `"
+          sizes="(min-width: 1280px) 960px, (min-width: 1040px) 832px, (min-width: 780px) 736px, (min-width: 680px) 576px, calc(94.44vw - 47px)"
           loading="lazy"
           class="img"
           :alt="'img-carrousel-' + index"
