4038e33c56dc7ebea3ec8e50a1ac4e2f4e3c68cd
Jonathan Alexis Bello Lopez <jonathan.bellofg@gmail.com>
2024-05-17 00:53:10 -0600

    add: robots txt and sitemap astro generate

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,27 @@ import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import vue from "@astrojs/vue";
 import tailwind from "@astrojs/tailwind";
-
 import netlify from "@astrojs/netlify";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), vue(), tailwind()],
+  site: "https://cenotegardens.com/",
+  integrations: [
+    react(),
+    vue(),
+    tailwind(),
+    sitemap({
+      i18n: {
+        defaultLocale: "en",
+        locales: {
+          en: "en-US",
+          es: "es-ES",
+        },
+      },
+    }),
+  ],
   output: "hybrid",
   adapter: netlify({
     cacheOnDemandPages: true,
